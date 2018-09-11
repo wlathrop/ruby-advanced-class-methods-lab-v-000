c354db86a6758834c3e3970470e5bec098b2f7c9
William Lathrop <wlathrop10@gmail.com>
2018-09-11 21:03:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,33 +11,32 @@ class Song
     self.class.all << self
   end
 
-end
-
   def self.create #class constructor
     song = self.new
     self.all << song
     song
   end
 
-  def self.new_by_name(title) #constructor
+  def self.new_by_name(title) #class constructor
     song = self.new
     song.name = title
     song
   end
 
-  def self.create_by_name(title) #constructor
+  def self.create_by_name(title) #class constructor
     song = self.create
     song.name = title
     song
   end
 
-  def self.find_by_name(title) #finder
+  def self.find_by_name(title) #class finder
     result = self.all.detect {|song| song.name == title}
     result
   end
 
   def self.find_or_create_by_name(title)
-        result = self.find_by_name(title)
+    #either return a matching song instance with that name or create a new song with the name and return the song instance
+    result = self.find_by_name(title)
     if result
       result
     else
